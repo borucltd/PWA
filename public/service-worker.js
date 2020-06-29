cc09a5e40f51c6c1a79f7f4f94e30f5d701031d7
borucltd <mboruc@DESKTOP-L3J83AR.localdomain>
2020-06-30 00:46:57 +0930

    Addedd better service-worker.js.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,10 @@
-const FILES_TO_CACHE = [
-    '/',
-    'index.html',
-    'index.js',
-    'manifest.json',
-    'style.css',
-    'icons/icon-192x192.png',
-    'icons/icon-512x512.png'
-  ];
-  
+const FILES_TO_CACHE =  ["/", 
+    "/index.html", 
+    "index.js",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png",
+    "manifest.json",
+]  
 
   
   const CACHE_NAME = "static-cache-v2";
