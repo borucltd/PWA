a8ead7208e8e4f025c9a90ff8c6ae6c863492f36
borucltd <mboruc@DESKTOP-L3J83AR.localdomain>
2020-06-30 20:57:28 +0930

    Heroku.

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 4000;
-
+const PORT = process.env.PORT || 4000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+console.log(process.env)
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
